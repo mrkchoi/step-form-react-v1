1a93d57623a24b0cd1587371544d6311c1fa9b4b
Kenneth Choi <kennethichoi@gmail.com>
2018-12-18 21:11:33 -0800

    multistep form with confirmation & success components

---
+++ src/components/FormPersonalDetails.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
+
+export class FormPersonalDetails extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+  render() {
+    const { values, handleChange } = this.props;
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Enter Your Occupation" />
+          <TextField
+            hintText="Enter Your Occupation"
+            floatingLabelText="Occupation"
+            onChange={handleChange("occupation")}
+            defaultValue={values.occupation}
+          />
+          <TextField
+            hintText="Enter Your City"
+            floatingLabelText="City"
+            onChange={handleChange("city")}
+            defaultValue={values.city}
+          />
+          <TextField
+            hintText="Enter Your Bio"
+            floatingLabelText="Bio"
+            onChange={handleChange("bio")}
+            defaultValue={values.bio}
+          />
+          <br />
+          <RaisedButton
+            label="Previous"
+            primary={false}
+            style={styles.button}
+            onClick={this.back}
+          />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
+const styles = {
+  button: {
+    margin: 15
+  }
+};
+
+export default FormPersonalDetails;
